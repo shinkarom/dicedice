44bf14c1341750cbb5770c8cb60bd3894a48e311
shinkarom <shinkarom@gmail.com>
2018-12-02 09:25:24 +0200

    First "Anything" logic.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+Array.prototype.random = function(){
+	return this[Math.floor(Math.random()*this.length)];
+}
+
+function redditAnything(){
+	return 'https://reddit.com/r/random/random';
+}
+
+function wikimediaAnything(){
+	return 'https://en.wikipedia.org/wiki/Special:Random';
+}
+
+function anythingClick(){
+	var items = [redditAnything, wikimediaAnything];
+	window.open(items.random()(),'_blank');
+}
+
 function bodyLoad(){
-	
+	document.getElementById('anything').addEventListener('click',anythingClick);
 }
